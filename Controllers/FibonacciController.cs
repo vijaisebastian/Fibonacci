9f8ae96c0ecc7388dc67e6e9c76c96238af9121c
vijai <vijai@DESKTOP-HJESG45>
2022-05-18 18:50:50 +0100

    Fibonacci app commit

--- Controllers/FibonacciController.cs
+++ Controllers/FibonacciController.cs
@@ -1,5 +1,4 @@
-﻿using Fibonacci.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +8,6 @@ namespace Fibonacci.Controllers
 {
     public class FibonacciController : Controller
     {
-        // GET: Fibonacci
         public ActionResult Index()
         {
             return View();
@@ -17,52 +15,25 @@ namespace Fibonacci.Controllers
 
         public ActionResult GenerateFibonacci(int FibonacciLength)
         {
-            
-            return View(GetFibonacci(FibonacciLength));
-        }
-        public FibonacciOperationsModel GetFibonacci(int p_FibonacciLength)
-        {
-            int intFirstNum = 1;
-            int intSecondNum = 2;
-            int[] ArrResult = new int[p_FibonacciLength];
-            ArrResult[0] = intFirstNum;
-            ArrResult[1] = intSecondNum;
-            int[] ArrEven = new int[p_FibonacciLength]; //To store even numbers from fibonacci series
-            FibonacciOperationsModel objFibonacciOperationsModel = new FibonacciOperationsModel();
-            for (int i = 2; i < p_FibonacciLength; i++)
+            BLayer.FibonacciControllerBL objFibonacciControllerBL = new BLayer.FibonacciControllerBL();
+            Model.FibonacciOperationsViewModel objFibonacciOperationsViewMode = new Model.FibonacciOperationsViewModel();
+
+            try
             {
-
-                ArrResult[i] = intFirstNum + intSecondNum;
-
-                if (CheckFibonacciIsValid(ArrResult[i]))
-                {
-                    if (CheckDigitIsEven(ArrResult[i]))
-                    {
-                        ArrEven[i] = ArrResult[i];
-                    }
-                    intFirstNum = intSecondNum;
-                    intSecondNum = ArrResult[i];
-                }
-                else
-                {
-                    ModelState.AddModelError(nameof(objFibonacciOperationsModel.FibonacciSeries), "Fibonacci sequence do not exceed four million.Invalid values found.Please try a lower value");
-                }
+                var result = objFibonacciControllerBL.GetFibonacci(FibonacciLength);
+                objFibonacciOperationsViewMode.FibonacciSeries = result.FibonacciSeries;
+                objFibonacciOperationsViewMode.SumOfEvens = result.SumOfEvens;
             }
-            objFibonacciOperationsModel.FibonacciSeries = ArrResult;
-            objFibonacciOperationsModel.SumOfEvens = ArrEven.Sum();
-            return objFibonacciOperationsModel;
-        }
-        public bool CheckDigitIsEven(int p_FibonacciNumber)
-        {
-            bool blnDigitIsEven = p_FibonacciNumber % 2 == 0 ? true:false;
-            return blnDigitIsEven;
-        }
-        public bool CheckFibonacciIsValid(int p_FibonacciNumber)
-        {
-            bool blnDigitIsValid = p_FibonacciNumber < 4000000 ? true :false;
-            return blnDigitIsValid;
-        }
+            catch (InvalidOperationException e)
+            {
+                ModelState.AddModelError(nameof(objFibonacciOperationsViewMode.FibonacciSeries), e.Message );
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
 
-
+            return View(objFibonacciOperationsViewMode);
+        }
     }
 }
---
+++ Model/FibonacciOperationsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fibonacci.Model
+{
+    public class FibonacciViewModel
+    {
+        [System.ComponentModel.DataAnnotations.Display(Name = "Size of Fibonacci Series")]
+        public int FibonacciLength { get; set; }
+    }
+    public class FibonacciOperationsViewModel
+    {
+        [System.ComponentModel.DataAnnotations.Display(Name = "Sum of the even-valued terms")]
+        public int SumOfEvens { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Display(Name = "Fibonacci Series")]
+        public int[] FibonacciSeries { get; set; }
+
+    }
+}
